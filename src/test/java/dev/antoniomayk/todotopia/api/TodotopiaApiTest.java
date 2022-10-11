db3c7bd7aac8c6dad181c4a79c7fb3f95332cbb6
antoniomayk <antoniomayk.g@gmail.com>
2022-10-11 16:51:46 -0300

    TAPI-1 Initial commit

---
+++ src/test/java/dev/antoniomayk/todotopia/api/TodotopiaApiTest.java
@@ -0,0 +1,20 @@
+package dev.antoniomayk.todotopia.api;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import jakarta.inject.Inject;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+@MicronautTest
+class TodotopiaApiTest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
